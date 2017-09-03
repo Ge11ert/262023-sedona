d595a13496d9f2c2e2ee64200a2a9aaef5a00999
Artyom <gellert94@mail.ru>
2017-09-04 01:01:52 +0300

    Правки стилизации и скрипта
    Поправлены состояния стрелок сортировки.
    В скрипт добавлена строка задания класса hidden по умолчанию для формы

--- js/script.js
+++ js/script.js
@@ -1,27 +1,54 @@
 var searchForm = document.querySelector(".search-hotel__popup");
 var searchButton = document.querySelector(".search-hotel__button");
-var lessButton = document.querySelector(".amount-container .less-button");
-var moreButton = document.querySelector(".amount-container .more-button");
+var lessButton = document.querySelectorAll(".amount-container .less-button");
+var moreButton = document.querySelectorAll(".amount-container .more-button");
 
 var adultInput = document.getElementById("adult-amount");
-var adultTotal = adultInput.value;
+var childrenInput = document.getElementById("children-amount");
 
+var adults = {
+  adults__lessButton: lessButton[0],
+  adults__moreButton: moreButton[0],
+  adultTotal: adultInput.value
+};
+
+var childrens = {
+  childrens__lessButton: lessButton[1],
+  childrens__moreButton: moreButton[1],
+  childrenTotal: childrenInput.value
+}
+
+searchForm.classList.add("visually-hidden");
 
 searchButton.addEventListener("click", function(e) {
   e.preventDefault();
   searchForm.classList.toggle("visually-hidden");
 });
 
-lessButton.addEventListener("click", function(e) {
-  if (adultTotal != 0) {
-    adultInput.value = --adultTotal;
+adults.adults__lessButton.addEventListener("click", function(e) {
+  if (adults.adultTotal != 0) {
+    adultInput.value = --adults.adultTotal;
   }
-  console.log(adultTotal);
-  return adultTotal;
+  console.log(adults.adultTotal);
+  return adults.adultTotal;
 })
 
-moreButton.addEventListener("click", function(e) {
-  adultInput.value = ++adultTotal;
-  console.log(adultTotal);
-  return adultTotal;
+adults.adults__moreButton.addEventListener("click", function(e) {
+  adultInput.value = ++adults.adultTotal;
+  console.log(adults.adultTotal);
+  return adults.adultTotal;
+})
+
+childrens.childrens__lessButton.addEventListener("click", function(e) {
+  if (childrens.childrenTotal != 0) {
+    childrenInput.value = --childrens.childrenTotal;
+  }
+  console.log(childrens.childrenTotal);
+  return childrens.childrenTotal;
+})
+
+childrens.childrens__moreButton.addEventListener("click", function(e) {
+  childrenInput.value = ++childrens.childrenTotal;
+  console.log(childrens.childrenTotal);
+  return childrens.childrenTotal;
 })
